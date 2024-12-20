3a01e636502872c990f02da3250bb0bd42a60f48
soun Soratha <121934976+ZensYing@users.noreply.github.com>
2024-12-20 19:48:37 +0700

    [create] project tab

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="bg-white  text-black dark:bg-dark dark:text-white ">
     <IndexHero />
     <PageIndexSkills />
+    <PageIndexProjectTabs />
     <!-- Timeline Section -->
 
   </div>
